5a0721f143bdde4a783b82bc58da98511f2bb32f
LuisP <luisjavierpolancomorel@gmail.com>
2021-05-16 20:49:37 -0400

    docs folder created and items moved into it

---
+++ docs/precache-manifest.05dffc93f354c3a6a2456fd097c838d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e6bd475ac8662f308dcd63b28a0b56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d14a665b4a2837ee943e",
+    "url": "/static/css/main.2a13a312.chunk.css"
+  },
+  {
+    "revision": "8283067c34de10f309a7",
+    "url": "/static/js/2.5ed246f1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.5ed246f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14a665b4a2837ee943e",
+    "url": "/static/js/main.f2bb0470.chunk.js"
+  },
+  {
+    "revision": "052bb53e0a7baafef9bd",
+    "url": "/static/js/runtime-main.144787eb.js"
+  },
+  {
+    "revision": "9a99295173be6c0f6dbc1e173d75ce48",
+    "url": "/static/media/bg-pattern-desktop.9a992951.svg"
+  },
+  {
+    "revision": "003b0c9744b45dea37e19234916edb43",
+    "url": "/static/media/hero-desktop.003b0c97.jpg"
+  },
+  {
+    "revision": "1d55799afb7254685302a38efea12fd1",
+    "url": "/static/media/hero-mobile.1d55799a.jpg"
+  },
+  {
+    "revision": "69950c2de382a8d4924260343e516fad",
+    "url": "/static/media/icon-arrow.69950c2d.svg"
+  },
+  {
+    "revision": "337809979308399afb36fa5f0e3fbcdf",
+    "url": "/static/media/icon-error.33780997.svg"
+  },
+  {
+    "revision": "877543ce69f4ed6f834f26e7dc4ca930",
+    "url": "/static/media/logo.877543ce.svg"
+  }
+]);
